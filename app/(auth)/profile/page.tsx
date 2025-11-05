11ee2ad6643cef424b59036c07504825ba807d20
alihassnain-github <155759112+alihassnain-github@users.noreply.github.com>
2025-11-05 12:08:24 +0500

    profile page created

---
+++ app/(auth)/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+        <main>
+            <section className="min-h-dvh px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 content-center">
+
+            </section>
+        </main>
+    )
+}
