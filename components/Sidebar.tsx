e55f2d816d1694d482c6524910f90d1806c30276
alihassnain-github <155759112+alihassnain-github@users.noreply.github.com>
2025-11-08 11:26:19 +0500

    sidebar created.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,37 +6,48 @@ import Link from "next/link";
 
 export default function Sidebar() {
 
-    const { isOpen } = useSidebar()!;
+    const { isOpen, handleToggle } = useSidebar()!;
 
     return (
-        <aside className={`z-70 fixed left-0 bg-base-200 h-full overflow-x-hidden transition-[width] duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] ${isOpen ? "w-64 border-r border-gray-200" : "w-0 border-r-0"}`}>
-            <ul className="menu lg:menu-lg bg-base-200 rounded-box w-full">
-                <li className="menu-title">Menu</li>
-                <li>
-                    <Link href={""} prefetch={false}>
-                        <RiHome4Line />
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href={""} prefetch={false}>
-                        <RiBookmarkLine />
-                        Library
-                    </Link>
-                </li>
-                <li>
-                    <Link href={""} prefetch={false}>
-                        <RiUser3Line />
-                        Profile
-                    </Link>
-                </li>
-                <li>
-                    <Link href={""} prefetch={false}>
-                        <RiArticleLine />
-                        Stories
-                    </Link>
-                </li>
-            </ul>
-        </aside>
+        <>
+
+            {/* Sidebar Overlay (for mobile) */}
+            {isOpen && (
+                <div
+                    className="fixed inset-0 bg-black/40 z-40 lg:hidden"
+                    onClick={handleToggle}
+                ></div>
+            )}
+
+            <aside className={`z-70 fixed left-0 bg-base-200 h-full overflow-x-hidden transition-[width] duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] ${isOpen ? "w-64 border-r border-gray-200" : "w-0 border-r-0"}`}>
+                <ul className="menu menu-md bg-base-200 rounded-box w-full">
+                    <li className="menu-title">Menu</li>
+                    <li>
+                        <Link href={""} prefetch={false}>
+                            <RiHome4Line size={18} />
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={""} prefetch={false}>
+                            <RiBookmarkLine size={18} />
+                            Library
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={""} prefetch={false}>
+                            <RiUser3Line size={18} />
+                            Profile
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={""} prefetch={false}>
+                            <RiArticleLine size={18} />
+                            Stories
+                        </Link>
+                    </li>
+                </ul>
+            </aside>
+        </>
     )
 }
--- context/sidemenu-provider.tsx
+++ context/sidemenu-provider.tsx
@@ -22,10 +22,8 @@ export default function SidebarProvider({ children }: { children: ReactNode }) {
         <SidebarContext.Provider value={{ isOpen, handleToggle }}>
             <Navbar />
             <div className="flex">
-                <div>
-                    <Sidebar />
-                </div>
-                <div className={`flex-1 ${isOpen ? "ms-64" : "ms-0"} transition-[margin] duration-500 ease-[cubic-bezier(0.22,1,0.36,1)]`}>
+                <Sidebar />
+                <div className={`flex-1 ${isOpen ? "lg:ms-64" : "lg:ms-0"} transition-[margin] duration-500 ease-[cubic-bezier(0.22,1,0.36,1)]`}>
                     <main>{children}</main>
                 </div>
             </div>
